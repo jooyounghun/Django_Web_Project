1eb98ece3e16001fad5fd85a4b1accbd8e55503b
Ubuntu <ubuntu@ip-172-31-24-238.ap-northeast-2.compute.internal>
2021-02-16 00:07:47 +0000

    first commit

---
+++ django_test/mytestsite/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'eat_db',
+            'USER': 'newuser',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '3306',
+            }
+        }
+SECRET_KEY = '%6qc4we7=buv9i(apsdf_c#75usau^2_a#6tql1^m$un!l1r)x'
